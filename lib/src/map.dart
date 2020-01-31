14b6c5794105b8288b65dc4409c7907ff45cb796
Andrew Chen <yongjhih@gmail.com>
2020-01-31 13:01:23 -0800

    Add Map.mergeWith()

---
+++ lib/src/map.dart
@@ -0,0 +1,21 @@
+part of dartx;
+
+extension MapX<K, V> on Map<K, V> {
+  /// Return merged Map by [reduce] reducer
+  Map<K, V> mergeWith(Map<K, V> other, {V reduce(V that, V it),
+    bool putIfAbsent = true,
+  }) {
+    final that = this; // ignore shadowed var
+
+    other.forEach((k, v) {
+      if (that.containsKey(k)) {
+        that[k] = reduce?.call(that[k], v) ?? v;
+      } else {
+        if (putIfAbsent) {
+           that.putIfAbsent(k, () => v);
+        }
+      }
+    });
+    return that;
+  }
+}
---
+++ test/map_test.dart
@@ -0,0 +1,127 @@
+import 'package:test/test.dart';
+import 'package:dartx/dartx.dart';
+
+void main() {
+  group('MapX', () {
+    test('.mergeWith()', () {
+      final a = <String, int>{
+        "a": null,
+        "ab": null,
+        "abc": null,
+      };
+      final b = <String, int>{
+      "ab": 2,
+      };
+      final expected = <String, int>{
+        "a": null,
+        "ab": 2,
+        "abc": null,
+      };
+      expect(a.mergeWith(b), expected);
+    });
+    test('.mergeWith(empty)', () {
+      final a = <String, int>{
+        "a": null,
+        "ab": null,
+        "abc": null,
+      };
+      final b = <String, int>{};
+      final expected = <String, int>{
+        "a": null,
+        "ab": null,
+        "abc": null,
+      };
+      expect(a.mergeWith(b), expected);
+    });
+    test('.mergeWith() from empty', () {
+      final a = <String, int>{};
+      final b = <String, int>{
+        "a": null,
+        "ab": null,
+        "abc": null,
+      };
+      final expected = <String, int>{
+        "a": null,
+        "ab": null,
+        "abc": null,
+      };
+      expect(a.mergeWith(b), expected);
+    });
+    test('.mergeWith(empty) from empty', () {
+      final a = <String, int>{
+      };
+      final b = <String, int>{
+      };
+      final expected = <String, int>{
+      };
+      expect(a.mergeWith(b), expected);
+    });
+    test('.mergeWith(reduce: (that, it))', () {
+      final a = <String, int>{
+        "a": 1,
+        "ab": 2,
+        "abc": 3,
+      };
+      final b = <String, int>{
+        "a": 1,
+        "ab": 2,
+        "abc": 3,
+      };
+      expect(
+        a.mergeWith(b, reduce: (that, it) => that + it),
+        <String, int>{
+          "a": 2,
+          "ab": 4,
+          "abc": 6,
+        }
+      );
+
+      expect(
+        <String, int>{
+          "a": 1,
+          "ab": 2,
+          "abc": 3,
+        }.mergeWith(<String, int>{
+          "ab": 2,
+        }, reduce: (that, it) => that + it),
+        <String, int>{
+          "a": 1,
+          "ab": 4,
+          "abc": 3,
+        }
+      );
+
+      expect(
+        <String, int>{
+          "a": 1,
+          "ab": 2,
+          "abc": 3,
+        }.mergeWith(<String, int>{
+          "abcd": 2,
+        }, reduce: (that, it) => that + it),
+        <String, int>{
+          "a": 1,
+          "ab": 2,
+          "abc": 3,
+          "abcd": 2,
+        }
+      );
+    });
+    test('.mergeWith(putIfAbsent: false)', () {
+      expect(
+        <String, int>{
+          "a": 1,
+          "ab": 2,
+          "abc": 3,
+        }.mergeWith(<String, int>{
+          "abcd": 2,
+        }, reduce: (that, it) => that + it, putIfAbsent: false),
+        <String, int>{
+          "a": 1,
+          "ab": 2,
+          "abc": 3,
+        }
+      );
+    });
+  });
+}
